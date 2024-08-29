88274ec6748ade99be6c5def59a8ffc940184c71
farisadlin <farisnura@gmail.com>
2024-08-29 14:17:28 +0700

    fix: unhide port

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 export const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || "3306"),
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   connectionLimit: 10,
-  // port: parseInt(process.env.DATABASE_PORT || "3306"),
 });
